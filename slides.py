a0ffda81026c370e2f4c0a9806856cfb59d796f2
Roy <davidroyapp@gmail.com>
2019-02-28 21:43:47 +0100

    slides.py

--- slides.py
+++ slides.py
@@ -10,25 +10,32 @@ class Photo(object):
     id2 = 0
     h = 'H'
     tags = []
+    tagSum = 0
+    score= 0
 
     def __init__(self, id1, id2, h, tags):
         self.id1 = id1
         self.id2 = id2
         self.h = h
         self.tags = tags
-
-class Slide(object):
-    photos = []
-
-    def __init__(self, photos):
-        self.photos = photos
+        self.tagSum = 0
+        self.score = 0
 
     def toString(self):
-        return ' '.join([str(x.id) for x in self.photos])
-    
+        if self.id1 == self.id2:
+            return str(self.id1)
+        else:
+            return str(self.id1)+' '+str(self.id2)
 
-def popVertical(photos):
-    return 
+class Slide(object):
+    photo = {}
+
+    def __init__(self, photo):
+        self.photo = photo
+
+    def toString(self):
+        return ' '+self.photo.toString()
+    
 
 
 #MAIN################################3
@@ -39,6 +46,7 @@ photoRaws = fh.readlines()
 fh.close()
 
 #prepare photos
+ALL = [] # todas las fotos
 H = [] # las fotos horizontales
 V = [] # las verticales
 V2 = [] # las verticales emparejadas
@@ -46,45 +54,55 @@ V2 = [] # las verticales emparejadas
 for i, photoRaw in enumerate(photoRaws):
     elements = photoRaw.replace('\n','',1).split()
     photo= Photo(i, i, elements[0], elements[2:len(elements)])
-    if (photo.h == "H")
+    if photo.h == "H":
         H.append(photo)
     else:
         V.append(photo)
 
 # Emparejar las V en V2
 
+
+# Juntarlas todas
+for photo in H:
+    ALL.append(photo)
+for photo in V2:
+    ALL.append(photo)
+
 # Calcular los tags más usados
-TAG = []
+TAG = {}
+for photo in H:
+    for tag in photo.tags:
+        if tag in TAG:
+            TAG[tag]+=1
+        else:
+            TAG[tag]=1
+
+for photo in V2:
+    for tag in photo.tags:
+        if tag in TAG:
+            TAG[tag]+=1
+        else:
+            TAG[tag]=1
+
+for photo in ALL:
+    for tag in photo.tags:
+        photo.tagSum+= TAG[tag]
+        photo.score = photo.tagSum
+
 
 # Calcular array con las fotos de mayor a menor nº de tags
+MAXTAGS = []
+
 
 #prepare slides
-slides = []
-for i, p1 in reversed(list(enumerate(photos))):
-    if i > 0:
-        popPhoto = photos.pop(len(photos) - 1)
-        slidePhotos = []
-        slidePhotos.append(popPhoto)
-        if popPhoto.h == 'V' and i > 0:
-            for j, p2 in reversed(list(enumerate(photos))):
-                if j > 0:
-                    if p2.h == 'V':
-                        slidePhotos.append(photos.pop(len(photos)-1))
-                        break
-        if (slidePhotos[0].h == 'V' and len(slidePhotos) == 2) or slidePhotos[0].h == 'H':
-            slides.append(Slide(slidePhotos))
-
-
-slidesOrdered = []
-
-slidesOrdered = slides
-
+ORDERED= []
+ORDERED= sorted(ALL, key=lambda x: x.socre,reverse=True)
 
 #write slides
 fw = open(outFilename[X], "w")
-fw.write(str(len(slidesOrdered)))
+fw.write(str(len(ORDERED)))
 fw.write('\n')
-for slide in slidesOrdered:
+for slide in ORDERED:
     fw.write(slide.toString())
     fw.write('\n')
 fw.close()
