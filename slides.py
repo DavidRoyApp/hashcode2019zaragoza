fa123d931e0bfc639747771d4796e997fea0efac
David Roy <davidroyapp@gmail.com>
2019-08-07 12:25:52 +0200

    FEATURES: subir cambios que  formateo c:

--- slides.py
+++ slides.py
@@ -1,133 +1,272 @@
 #!/usr/bin/python
+
 # -*- coding: utf-8 -*-
 
-inFilename = ["a_example.txt", "b_lovely_landscapes.txt", "c_memorable_moments.txt", "d_pet_pictures.txt", "e_shiny_selfies.txt"]
-outFilename = ["a_output.txt", "b_output.txt", "c_output.txt", "d_output.txt", "e_output.txt"]
 
 
-class Photo(object):
-    id1 = 0
-    id2 = 0
-    h = 'H'
-    length = 0
-    tags = []
-    tagSum = 0
-    score= 0
+import numpy as np
 
-    def __init__(self, id1, id2, h, length, tags):
-        self.id1 = id1
-        self.id2 = id2
-        self.h = h
-        self.length = length
-        self.tags = tags
-        self.tagSum = 0
-        self.score = 0
+from scipy import sparse
 
-    def toString(self):
-        if self.id1 == self.id2:
-            return str(self.id1)
-        else:
-            return str(self.id1)+' '+str(self.id2)
+from datetime import datetime
 
-class Slide(object):
-    photo = {}
 
-    def __init__(self, photo):
-        self.photo = photo
 
-    def toString(self):
-        return ' '+self.photo.toString()
-    
+inFilename = [
 
-# Calcular array con las fotos de mayor a menor nº de tags
-def pairVertical(vPhotos):
-    Pairs = []
-    for i, photo in enumerate(vPhotos):
-        bestScore = 0
-        bestMatch = Photo
-        matchedIds = []
-        for check in vPhotos[i+1:len(vPhotos)]:
-            if (check.id1 in matchedIds):
-                continue
-            commontags = sumTags(photo, check)
-            lencommontags = len(commontags)
-            if(bestScore >= photo.length+check.length):
-                Pairs.append(Photo(photo.id1, bestMatch.id1, photo.h, lencommontags, commontags))
-                matchedIds.append(photo.id1)
-                matchedIds.append(check.id1)
-                break
-            else:
-                score = lencommontags
-                if(bestScore < score):
-                    bestScore = score
-                    bestMatch = check
-    return Pairs
+    "a_example.txt",
 
-def matchingTags(photo1, photo2):
-    return len(photo1.tags.intersection(photo2.tags))
+    "b_lovely_landscapes.txt",
 
-def sumTags(photo1, photo2):
-    return list(set(photo1.tags + photo2.tags))
+    "c_memorable_moments.txt",
 
-#MAIN################################3
-X=3 #0: el fichero a_xxx.txt , 1 el fichero b_xxx.txt
-fh = open(inFilename[X], "r")
-totalPhotos = fh.readline()
-photoRaws = fh.readlines()
-fh.close()
+    "d_pet_pictures.txt",
 
-#prepare photos
-ALL = [] # todas las fotos
+    "e_shiny_selfies.txt"]
+
+outFilename = [
+
+    "a_output.txt",
+
+    "b_output.txt",
+
+    "c_output.txt",
+
+    "d_output.txt",
+
+    "e_output.txt"]
+
+
+
+################################################################################
+
+N = 0 # número de fotos
+
 H = [] # las fotos horizontales
+
 V = [] # las verticales
-V2 = [] # las verticales emparejadas
 
-for i, photoRaw in enumerate(photoRaws):
-    elements = photoRaw.replace('\n','',1).split()
-    photo= Photo(i, i, elements[0], elements[1], elements[2:len(elements)])
-    if photo.h == "H":
-        H.append(photo)
-    else:
-        V.append(photo)
+FINAL = [] # el resultado final
 
-# Emparejar las V en V2
+TAGS = {} # tag_string => tag_int
 
-V_sorted= sorted(V, key=lambda x: x.length,reverse=True)
-V2 = pairVertical(V_sorted)
+TAGN = 0
 
-# Juntarlas todas
-for photo in H:
-    ALL.append(photo)
-for photo in V2:
-    ALL.append(photo)
+MATRIX = []
 
-# Calcular los tags más usados
-TAG = {}
-for photo in ALL:
-    for tag in photo.tags:
-        if tag in TAG:
-            TAG[tag]+=1
-        else:
-            TAG[tag]=1
+IDS = []
 
-for photo in ALL:
-    for tag in photo.tags:
-        photo.tagSum+= TAG[tag]
+LENS = []
 
 
-#prepare slides
-for photo in ALL:
-    for tag in photo.tags:
-        photo.score = photo.tagSum + photo.tags.length
 
-ORDERED= sorted(ALL, key=lambda x: x.score,reverse=True)
+################################################################################
 
-#write slides
-fw = open(outFilename[X], "w")
-fw.write(str(len(ORDERED)))
-fw.write('\n')
-for slide in ORDERED:
-    fw.write(slide.toString())
-    fw.write('\n')
-    print slide.toString()
-fw.close()
+def main():
+
+    for i in range(1, 2):
+
+        print("START")
+
+        readInput(inFilename[i])
+
+        print("RUNNING...")
+
+        algorithm()
+
+        print("FINISH")
+
+        writeOutput(outFilename[i])
+
+
+
+
+
+def readInput(filename):
+
+    global N, H, V, TAGS, TAGN, MATRIX, IDS, LENS
+
+
+
+    with open(filename) as f:
+
+        lines = f.readlines()
+
+    lines = [x.strip() for x in lines]
+
+
+
+    # line 1
+
+    N = int(lines[0].split()[0])
+
+    lines.pop(0)
+
+    LENS= np.zeros((N,1), dtype=np.int)
+
+
+
+    # line 2..n
+
+    indexesX=[]
+
+    indexesY=[]
+
+    for i, line in enumerate(lines):
+
+        fields= line.split()
+
+        LENS[i][0]= int(fields[1])
+
+        tags= fields[2:]
+
+        # en TAGS se indexa por tag y devuelve el código
+
+        for tag in tags:
+
+            if tag not in TAGS:
+
+                TAGS[tag]=TAGN
+
+                TAGN+=1
+
+            indexesX.append(i)
+
+            indexesY.append(TAGS[tag])
+
+    data= np.ones(len(indexesX), dtype=np.int8)
+
+    MATRIX= sparse.coo_matrix((data, (indexesX, indexesY))).tocsr()
+
+    IDS= np.arange(N)
+
+
+
+def writeOutput(filename):
+
+    file = open(filename,"w") 
+
+  
+
+    file.write(str(len(FINAL)))
+
+    file.write('\n')
+
+    for item in FINAL:
+
+        file.write(str(item))
+
+        file.write('\n')
+
+    file.close()
+
+
+
+
+
+################################################################################
+
+def algorithm():
+
+    global N, TAGN, H, V, FINAL, MATRIX, IDS, LENS
+
+
+
+    totalscore= 0
+
+    i= 0
+
+    FINAL.append(0)
+
+    while np.size(MATRIX, axis=0) > i:
+
+        print("-"*20)
+
+
+
+        row_i= MATRIX[i].todense()
+
+        len_i= LENS[i]
+
+        IDS= np.delete(IDS, i)
+
+        LENS= np.delete(LENS, i, axis= 0)
+
+
+
+#        s= datetime.now()
+
+        MATRIX= sparse.vstack([MATRIX[:i,:], MATRIX[i+1:,:]])
+
+#        print(datetime.now()-s)
+
+
+
+#        s= datetime.now()
+
+        equals= MATRIX.multiply(row_i)
+
+#        print(datetime.now()-s)
+
+
+
+#        s= datetime.now()
+
+        sum_equals= equals.sum(axis=1)
+
+#        print(datetime.now()-s)
+
+
+
+        #sum_diffsA= -sum_equals + np.count_nonzero(row_i)
+
+        sum_diffsA= -sum_equals + len_i
+
+
+
+        #s= datetime.now()
+
+        #row_inv= (~(row_i.astype(np.bool))).astype(np.int8)
+
+        #diffsB= MATRIX.multiply(row_inv)
+
+        #print(datetime.now()-s)
+
+
+
+        #sum_diffsB= diffsB.sum(axis=1)
+
+        sum_diffsB= -sum_equals + LENS
+
+
+
+        sums= np.column_stack((sum_equals, sum_diffsA, sum_diffsB))
+
+        scores= np.amin(sums, axis=1)
+
+        next_i= np.argmax(scores)
+
+        score= np.max(scores)
+
+        totalscore+= score
+
+        i= next_i
+
+        FINAL.append(IDS[i])
+
+        print(str(np.size(MATRIX, axis=0))+": "+str(IDS[i])+" "+str(score)+" points")
+
+    print("Total score: "+str(totalscore))
+
+
+
+
+
+################################################################################
+
+# esto siempre al final
+
+if __name__ == '__main__':
+
+    main()
+
